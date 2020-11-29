1f49cd4667a7013d9f587abf2960f13e654d7719
ngowilliam1 <46735044+ngowilliam1@users.noreply.github.com>
2020-11-29 15:09:56 -0500

    Update biharmonic_distance.cpp

--- src/biharmonic_distance.cpp
+++ src/biharmonic_distance.cpp
@@ -7,11 +7,12 @@ void biharmonic_distance(
   const int K,
   Eigen::MatrixXd &D)
 {   
+    D.resize(V.rows(), V.rows());
     D.setZero();
     Eigen::MatrixXd desiredEigenVectors;
     Eigen::RowVectorXd desiredEigenValues;
     biharmonic_eigens(V, F, K, desiredEigenVectors, desiredEigenValues);
-    D.resize(V.rows(), V.rows());
+    
     
     for (int i = 0; i < V.rows(); i++){
         for (int j = (i+1); j < V.rows(); j++){
